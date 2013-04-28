54a88e97af0547cccd518132a4ec2a5ed8a2bbc4
Andrew Berls <andrew.berls@gmail.com>
2013-04-28 14:16:37 -0700

    Update bounce example with comments

--- examples/bounce/build/bounce.js
+++ examples/bounce/build/bounce.js
@@ -1,52 +1,60 @@
-var __hasProp = {}.hasOwnProperty,
-  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+(function() {
+  var __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-Kona.ready(function() {
-  var Shape, main;
-  Kona.Canvas.init('canvas');
-  main = new Kona.Scene({
-    name: 'main',
-    active: true
+  Kona.ready(function() {
+    var Shape, main, square;
+    Kona.Canvas.init('gameCanvas');
+    main = new Kona.Scene({
+      name: 'main',
+      active: true
+    });
+    Shape = (function(_super) {
+
+      __extends(Shape, _super);
+
+      function Shape(opts) {
+        if (opts == null) {
+          opts = {};
+        }
+        opts.group || (opts.group = 'shapes');
+        Shape.__super__.constructor.call(this, opts);
+        this.gravity = false;
+      }
+
+      Shape.prototype.update = function() {
+        Shape.__super__.update.call(this);
+        if (this.left() <= 0 || this.right() >= Kona.Canvas.width) {
+          this.direction.dx *= -1;
+        }
+        if (this.top() <= 0 || this.bottom() >= Kona.Canvas.height) {
+          return this.direction.dy *= -1;
+        }
+      };
+
+      Shape.prototype.draw = function() {
+        var _this = this;
+        return Kona.Canvas.safe(function() {
+          return Kona.Canvas.drawRect(_this.position, _this.box, {
+            color: 'blue'
+          });
+        });
+      };
+
+      return Shape;
+
+    })(Kona.Entity);
+    square = new Shape({
+      x: 200,
+      y: 200,
+      width: 20,
+      height: 20,
+      dx: -2,
+      dy: -1,
+      speed: 3
+    });
+    main.addEntity(square);
+    return Kona.Engine.start();
   });
-  Shape = (function(_super) {
 
-    __extends(Shape, _super);
-
-    function Shape() {
-      return Shape.__super__.constructor.apply(this, arguments);
-    }
-
-    Shape.prototype.update = function() {
-      this.position.x += this.speed * this.direction.dx;
-      this.position.y += this.speed * this.direction.dy;
-      if (this.left() < 0 || this.right() > Kona.Canvas.width) {
-        this.direction.dx *= -1;
-      }
-      if (this.top() < 0 || this.bottom() > Kona.Canvas.height) {
-        return this.direction.dy *= -1;
-      }
-    };
-
-    Shape.prototype.draw = function() {
-      var _this = this;
-      return Kona.Canvas.safe(function() {
-        Kona.Canvas.ctx.fillStyle = 'blue';
-        return Kona.Canvas.ctx.fillRect(_this.position.x, _this.position.y, _this.box.width, _this.box.height);
-      });
-    };
-
-    return Shape;
-
-  })(Kona.Entity);
-  main.addEntity(new Shape({
-    x: 200,
-    y: 200,
-    width: 20,
-    height: 20,
-    dx: -2,
-    dy: -1,
-    speed: 3,
-    group: 'ball'
-  }));
-  return Kona.Engine.start();
-});
+}).call(this);
--- examples/bounce/src/bounce.coffee
+++ examples/bounce/src/bounce.coffee
@@ -1,22 +1,54 @@
+# Wrap everything in Kona.ready callback
+# to run when the browser finishes loading
 Kona.ready ->
 
-  Kona.Canvas.init('canvas')
+  # Initialize the main canvas with the id of our `<canvas>` element
+  Kona.Canvas.init('gameCanvas')
 
+
+  # This example has just one scene, which will start rendering
+  # as soon as the engine starts
   main = new Kona.Scene { name: 'main', active: true }
 
+
+
+  # Our main shape that will be bouncing around the screen
+  # All distinct entities on the screen should inherit from `Kona.Entity`
   class Shape extends Kona.Entity
+    # Our constructor mostly just passes things through to the main
+    # Entity constructor, but we explicitly turn off gravity for this object
+    # Setting the group here prevents us from having to specify it when we construct
+    # instances of this class
+    constructor: (opts={}) ->
+      opts.group ||= 'shapes'
+      super(opts)
+      @gravity = false
+
+
+    # `update()` is responsible for changing the object's state
+    # (position, direction, etc) as necessary.
     update: ->
-      @position.x += (@speed * @direction.dx)
-      @position.y += (@speed * @direction.dy)
+      super()
 
-      @direction.dx *= -1 if @left() < 0 || @right() > Kona.Canvas.width
-      @direction.dy *= -1 if @top()  < 0 || @bottom() > Kona.Canvas.height
+      # Reverse direction if we hit a wall on any side
+      @direction.dx *= -1 if @left() <= 0 || @right()  >= Kona.Canvas.width
+      @direction.dy *= -1 if @top()  <= 0 || @bottom() >= Kona.Canvas.height
 
+
+    # `draw()` is responsible for rendering the object to the screen
+    # Here, we draw a simple rectangle using Kona's `drawRect` method
+    # which is just an easier way of calling `canvas.ctx.fillRect()`
     draw: ->
       Kona.Canvas.safe =>
-        Kona.Canvas.ctx.fillStyle = 'blue'
-        Kona.Canvas.ctx.fillRect(@position.x, @position.y, @box.width, @box.height)
+        Kona.Canvas.drawRect(@position, @box, color: 'blue')
 
-  main.addEntity(new Shape { x: 200, y: 200, width: 20, height: 20, dx: -2, dy: -1, speed: 3, group: 'ball' })
 
+
+  # Construct a shape object with some initial direction/speed and
+  # add it to the scene
+  square = new Shape { x: 200, y: 200, width: 20, height: 20, dx: -2, dy: -1, speed: 3 }
+  main.addEntity(square)
+
+
+  # At this point, we're ready to start the engine!
   Kona.Engine.start()
